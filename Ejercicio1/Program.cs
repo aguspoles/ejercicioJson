36590b19c95e75e49f088700d7cb927598bf4b55
68 <68>
2017-04-21 12:56:57 -0300

    first commit

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+using Newtonsoft.Json.Linq;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest req = WebRequest.Create("http://www.omdbapi.com/?t=[Terminator]&");
+
+            WebResponse respuesta = req.GetResponse();
+
+            Stream stream = respuesta.GetResponseStream();
+
+            StreamReader sr = new StreamReader(stream);
+
+            /*string pasoAMinuscula = sr.ReadToEnd();
+            pasoAMinuscula.ToLower();*/
+            JObject data = JObject.Parse(sr.ReadToEnd());
+            Console.WriteLine((string)data["Titulo"]);
+            string titulo;
+            Console.Write("Introduzca el nombre de la pelicula:");
+            titulo = Console.ReadLine();
+
+            //string titulo = (string)data[año];
+
+            Console.WriteLine(titulo);
+
+            Console.ReadKey();
+        }
+
+    }
+}
